d1003110bd30d3abecdcd813b382cd624d273d67
Henry <henry.kuo@eyesmedia.com.tw>
2018-12-24 02:49:17 -0600

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -28,7 +28,7 @@ function initializeApp(data) {
 
     // sendMessages call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        console.log('messageSend');
+        console.log('messageSend123');
         liff.sendMessages([{
             type: 'text',
             text: "You've successfully sent a message! Hooray!"
